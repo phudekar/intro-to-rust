68b054983b1155200dca0e3cc3ed814159ba7c2f
Pradip Hudekar <pradiph@sahajsoft.com>
2020-07-15 00:17:11 +0530

    added journeys

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,18 @@
-mod complex_types;
-mod library;
-
-use complex_types::Person;
-use library::Library;
+mod transport;
+use transport::{Journey, Metro, MetroLine};
 
 fn main() {
-    let mut library = Library::new();
-    let title = "Introduction to Rust";
-    let book = library::Book::new(title, "Community", 10.0);
-    library.add_book(book);
+    let from = "Swargate";
+    let to = "Viman Nagar";
+    let journey = Journey::new(from, to);
+    let metro = Metro {
+        line: MetroLine::Red,
+    };
     println!(
-        "Book available: {}",
-        if library.is_book_available(title) {
-            "Yes"
-        } else {
-            "No"
-        }
-    );
-
-    let person = Person::new_student("Ferris", 15, "FR_1");
-    let (book, cost) = library
-        .borrow(title, person.clone())
-        .expect("Expected book after borrowing");
-    let currency = '₹';
-    println!(
-        "{} is borrowed by {} for {}{}",
-        book.title, person.name, currency, cost
+        "Fare from {} to {} by {:?} Line is {}",
+        from,
+        to,
+        MetroLine::Red,
+        journey.fare(&metro)
     )
 }
