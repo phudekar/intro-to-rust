9f59402c8c0af1c5a7c9a7c0e3c3b0d3593d92bc
Pradip Hudekar <pradiph@sahajsoft.com>
2020-07-15 12:56:24 +0530

    modified greet

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,17 @@
 fn main() {
-    let age = 7;
+    let mut age = 7;
+    age = 8;
+
     let teenager = is_teenager(age);
+    println!("Teenager: {}", teenager);
     println!("Age is {}", age);
 
-    // let name = String::from("Ferris");
-    // let length = length(name);
-    // println!("Name is {}", name);
+    let mut name = String::from("Ferris");
+    let length = length(&name);
+    println!("Length is {}", length);
+    println!("Name is {}", name);
+    greet(&mut name);
+    println!("{}", name);
 
     // let name = String::from("Ferris");
     // let length = length_with_borrow(&name);
@@ -16,7 +22,11 @@ fn is_teenager(age: i32) -> bool {
     age > 12 && age < 18
 }
 
-fn length(str: String) -> usize {
+fn greet(name: &mut String) {
+    name.push('H');
+}
+
+fn length(str: &String) -> usize {
     str.len()
 }
 
