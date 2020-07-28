3d1212794d0d0eacfa30c263219504465c5e7f28
Pradip Hudekar <pradiph@sahajsoft.com>
2020-07-28 18:54:26 +0530

    Revert "added threads sample"
    This reverts commit 8724ac69824543bad0b5261ae487927be7e4e076.

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,18 @@
-mod threads;
-use std::thread;
+mod transport;
+use transport::{Journey, Metro, MetroLine};
 
 fn main() {
-    let mut threads = vec![];
-    let mut count = 0;
-    for i in 1..=10 {
-        threads.push(thread::spawn(move || {
-            count += 1;
-            thread::sleep(std::time::Duration::from_secs(1));
-            println!("{} -> {}", i, count)
-        }));
-    }
-
-    for t in threads {
-        t.join().unwrap();
-    }
-
-    // threads::run();
+    let from = "Swargate";
+    let to = "Viman Nagar";
+    let journey = Journey::new(from, to);
+    let metro = Metro {
+        line: MetroLine::Red,
+    };
+    println!(
+        "Fare from {} to {} by {:?} Line is {}",
+        from,
+        to,
+        MetroLine::Red,
+        journey.fare(&metro)
+    )
 }
