12ccd6b7814a3bfe246bb8c7ed4dff42d6f6c7e7
Pradip Hudekar <pradiph@sahajsoft.com>
2020-07-29 14:56:55 +0530

    refactored pattern matching

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,5 @@
-mod sahaj;
-
-use sahaj::pattern_matching::Shape;
+mod pattern_matching;
 
 fn main() {
-    let a = 5;
-    let location = Location {
-        latitude: 10.0,
-        longitude: 20.0,
-    };
-    print(location);
-
-    let [first, second] = ['A', 'B'];
-    println!("First: {}, Second:{}", first, second);
-
-    let [first, .., last] = [1, 2, 3, 4];
-    println!("First: {}, last: {}", first, last);
-
-    let shape = Shape::Rectangle(5.0, 8.0);
-    println!("Area of rectangle is {}", shape.area())
-}
-
-fn print(location: Location) {
-    let Location {
-        latitude,
-        longitude,
-    } = location;
-    println!("Latitude: {}, Longitude: {}", latitude, longitude);
-}
-
-struct Location {
-    latitude: f32,
-    longitude: f32,
+    pattern_matching::run();
 }
--- src/pattern_matching.rs
+++ src/pattern_matching.rs
@@ -1,5 +1,36 @@
 use std::f64::consts::PI;
 
+pub fn run() {
+    let a = 5;
+    let location = Location {
+        latitude: 10.0,
+        longitude: 20.0,
+    };
+    print(location);
+
+    let [first, second] = ['A', 'B'];
+    println!("First: {}, Second:{}", first, second);
+
+    let [first, .., last] = [1, 2, 3, 4];
+    println!("First: {}, last: {}", first, last);
+
+    let shape = Shape::Rectangle(5.0, 8.0);
+    println!("Area of rectangle is {}", shape.area())
+}
+
+fn print(location: Location) {
+    let Location {
+        latitude,
+        longitude,
+    } = location;
+    println!("Latitude: {}, Longitude: {}", latitude, longitude);
+}
+
+struct Location {
+    latitude: f32,
+    longitude: f32,
+}
+
 pub enum Shape {
     Circle(f64),
     Square(f64),
