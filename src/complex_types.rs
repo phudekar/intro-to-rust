8a401e09373c9dc63fabbc346bed5e274d88a066
Pradip Hudekar <pradiph@sahajsoft.com>
2020-07-29 14:49:53 +0530

    refactored complex_types

--- src/complex_types.rs
+++ src/complex_types.rs
@@ -1,3 +1,29 @@
+use library;
+pub fn run() {
+    let mut library = library::Library::new();
+    let title = "Introduction to Rust";
+    let book = library::Book::new(title, "Community", 10.0);
+    library.add_book(book);
+    println!(
+        "Book available: {}",
+        if library.is_book_available(title) {
+            "Yes"
+        } else {
+            "No"
+        }
+    );
+
+    let person = Person::new_student("Ferris", 15, "FR_1");
+    let (book, cost) = library
+        .borrow(title, person.clone())
+        .expect("Expected book after borrowing");
+    let currency = '₹';
+    println!(
+        "{} is borrowed by {} for {}{}",
+        book.title, person.name, currency, cost
+    )
+}
+
 #[derive(Clone)]
 pub struct Person {
     pub name: String,
--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,4 @@
 mod complex_types;
 mod library;
 
-use complex_types::Person;
-use library::Library;
-
-fn main() {
-    let mut library = Library::new();
-    let title = "Introduction to Rust";
-    let book = library::Book::new(title, "Community", 10.0);
-    library.add_book(book);
-    println!(
-        "Book available: {}",
-        if library.is_book_available(title) {
-            "Yes"
-        } else {
-            "No"
-        }
-    );
-
-    let person = Person::new_student("Ferris", 15, "FR_1");
-    let (book, cost) = library
-        .borrow(title, person.clone())
-        .expect("Expected book after borrowing");
-    let currency = '₹';
-    println!(
-        "{} is borrowed by {} for {}{}",
-        book.title, person.name, currency, cost
-    )
-}
+fn main() {}
